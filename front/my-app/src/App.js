95411909d3c093d18804031736b7f0ff86d9ab99
c4misado <adelmanalec@gmail.com>
2020-06-12 11:42:49 +0300

    did second quest

---
+++ front/my-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SignUp from './components/SignUp';
+
+function App() {
+  return (
+    <div className="App">
+
+      <SignUp email />
+      
+    </div>
+  );
+}
+
+export default App;
