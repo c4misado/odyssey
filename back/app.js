0a213b81a9e8571a420996354516a0d650958c85
c4misado <adelmanalec@gmail.com>
2020-06-15 12:32:40 +0300

    did fourth quest

---
+++ back/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const authRouter = require("./routes/auth/auth");
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+
+
+app.get("/", (req, res) => {
+    res.send("youhou (app)");
+})
+
+app.use('/auth', authRouter);
+
+// / in case path is not found, return the 'Not Found' 404 code
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// launch the node server
+let server = app.listen(process.env.PORT || 3015, function () {
+    console.log('Listening on port ' + server.address().port);
+});
